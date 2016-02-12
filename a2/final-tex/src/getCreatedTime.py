2e649d27f9a7afc77c724c526e629ff7e3bc43dc
unknown <smajeti@D-1103B-2.cs.odu.edu>
2016-02-11 20:55:35 -0500

    a2

---
+++ a2/final-tex/src/getCreatedTime.py
@@ -0,0 +1,40 @@
+'''
+CS532: Introduction to Web Science
+Author: Srividya Majeti
+Assignment 2
+'''
+
+import json
+import datetime 
+import dateutil.parser
+
+now = datetime.datetime.now()
+readCreatedTime = open('createdTime','r')
+ageFile=open('UriWithAge.json','w')
+noEstimatedDatecounter =0
+for line in readCreatedTime:
+	data= json.loads(line)
+	Age= {}
+	AgeList =[]
+	if len(data['Estimated Creation Date']) >0:
+		Age['uri'] = data['URI']
+		EstimatedDate= data['Estimated Creation Date']
+		d1 = dateutil.parser.parse(EstimatedDate)
+		nowDate =now.isoformat()
+		d2 = dateutil.parser.parse(nowDate)
+		days = abs((d2 - d1).days)
+		Age['days'] = days
+		ageFile.write(json.dumps(Age) + "\n")
+		# print Age
+	else:
+		noEstimatedDatecounter +=1
+ageFile.close()
+		
+print "URI's with no EstimatedDate",noEstimatedDatecounter
+
+read= open('UriWithAge.json','r')
+output= open('days.json','w')
+for line in read:
+	data=json.loads(line)
+	# print data['days']
+	output.write(str(data['days'])+"\n")
